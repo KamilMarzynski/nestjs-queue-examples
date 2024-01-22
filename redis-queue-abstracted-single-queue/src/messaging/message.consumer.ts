20f4b8f7a3167a3fc9ba30722e3c1ddbccf99474
KamilMarzynski <kamil.marzynski@gmail.com>
2024-01-22 20:00:29 +0100

    Refactor MessageConsumer to use MessageHandler interface
    This commit refactors the MessageConsumer class in the `message.consumer.ts` file to use the `MessageHandler` interface. It also adds logging statements for registering and processing message handlers.

--- redis-queue-abstracted-single-queue/src/messaging/message.consumer.ts
+++ redis-queue-abstracted-single-queue/src/messaging/message.consumer.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { MessageHandler } from './message.handler';
+import { Message } from './types';
 
 @Injectable()
 export class MessageConsumer {
-  private handlers: Record<string, ((...args) => Promise<any>)[]> = {};
+  private handlers: Record<string, MessageHandler[]> = {};
 
   public registerHandler({ messageName, handler }) {
+    console.log('Registering handler for message', messageName);
     if (!this.handlers[messageName]) {
       this.handlers[messageName] = [];
     }
     this.handlers[messageName].push(handler);
+    console.log('Registered handler for message', this.handlers);
   }
 
-  public async processMessageOnHandlers(message: any) {
+  public async processMessageOnHandlers<T>(message: Message<T>) {
     const handlers = this.handlers[message.name];
+    console.log('Processing message on handlers', message.name, handlers);
     if (!handlers) {
+      console.log('No handlers for message', message.name);
       return;
     }
-    handlers.forEach((handler) => {
-      handler(message.data);
-    });
+    return Promise.all(
+      handlers.map(async (handler: MessageHandler) => {
+        await handler.handle(message.data);
+      }),
+    );
   }
 }
